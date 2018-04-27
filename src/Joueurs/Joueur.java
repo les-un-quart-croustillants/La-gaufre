2c0678b98b626ca11672470e45e2365dbf5b8e9d
Louka Soret <louka.soret@gmail.com>
2018-04-27 11:26:08 +0200

    Ajout class generique joueur et declinaison en JoueurIA et JoueurPhysique

---
+++ src/Joueurs/Joueur.java
@@ -0,0 +1,79 @@
+package Joueurs;
+
+/**
+ * 
+ * @author Louka Soret
+ *
+ */
+public abstract class Joueur {
+	Plateau plateau;
+	int num;
+
+	
+	public enum Difficulte {
+		FACILE,
+		MOYEN,
+		DIFFICILE;
+		
+		static public String toString (Difficulte d) {
+			switch(d) {
+				case FACILE:
+					return "facile";
+				case MOYEN:
+					return "moyen";
+				case DIFFICILE:
+					return "difficile";
+				default:
+					return "unknown";
+			}
+		}
+		
+		static public Difficulte fromString (String s) {
+			switch(s) {
+				case "facile":
+					return FACILE;
+				case "moyen":
+					return MOYEN;
+				case "difficile":
+					return DIFFICILE;
+			}
+		}
+	}
+	/**
+	 * Constructeur pour la classe Joueur
+	 * 
+	 * @param p un plateau de jeu
+	 */
+	Joueur(Plateau p) {
+	        plateau = p;
+	    }
+
+	/**
+	 * Méthode appelée lorsque le tour d'un joueur vient
+	 * 
+	 * @return un temps à attendre en millisecondes ou 0 si aucune temporisation requise
+	 */
+	int delai() {
+		return 0;
+	}
+
+	/**
+	 * Méthode appelée une fois le temps écoulé
+	 * 
+	 * @return True si le traitement a reussi, False sinon
+	 */
+	boolean delaiEcoule() {
+		return false;
+	}
+
+	/**
+	 * Méthode appelée lors d'un clic sur le plateau
+	 * 
+	 * @param i absisse
+	 * @param j ordonnée
+	 * @return True si le coup a bien été joué, False si le coup n'était pas jouable
+	 */
+	boolean jouerCoup(int i, int j) {
+		return false;
+	}
+}
---
+++ src/Joueurs/JoueurPhysique.java
@@ -0,0 +1,22 @@
+package Joueurs;
+
+import Modele.Couple;
+import Modele.Plateau;
+
+public class JoueurPhysique extends Joueur{
+	
+	JoueurPhysique(Plateau p){
+		super(p);
+	}
+	
+	@Override
+	boolean jouerCoup(Couple pos) {
+        if (plateau.estMangeable(pos)) { // Test coup valide
+            plateau.jouer(pos);
+            return true;
+        } else {
+            return false;
+        }
+	}
+
+}
