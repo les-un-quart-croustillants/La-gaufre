e4a7d8f3e5f234754d266f259181853e7e729e06
Gaetan Sorin <contact@gaetansorin.com>
2018-04-29 11:24:27 +0200

    Ajout de Plateau.clone

--- src/Modele/Plateau.java
+++ src/Modele/Plateau.java
@@ -155,6 +155,10 @@ public class Plateau {
 		return compteurCoups;
 	}
 
+	public Plateau clone() {
+		return new Plateau(this.hauteur(), this.largeur(), this.getCompteurCoups(), this.getTab());
+	}
+
 	@Override
 	public String toString() {
 		String res = "[\n";
