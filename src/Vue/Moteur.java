051bede968f7343df9a989042734e1a527af614a
Thomas Vandendorpe <vandendt@F202-06.e-im2ag.ujf-grenoble.fr>
2018-04-30 10:59:24 +0200

    correction undo/redo

--- src/Vue/Moteur.java
+++ src/Vue/Moteur.java
@@ -10,7 +10,7 @@ import javafx.scene.paint.Color;
 
 public class Moteur {
 	public enum FSA_state {
-		DEBUT_TOUR(0), CHOISIR_COUP(1), CASE_CIBLE(2), FIN_COUP(3), FIN_TOUR(4);
+		DEBUT_TOUR(0), CHOISIR_COUP(1), CASE_CIBLE(2), FIN_COUP(3), FIN_TOUR(4), FIN_PARTIE(5);
 
 		public int value;
 
@@ -21,14 +21,14 @@ public class Moteur {
 
 	public Plateau plateau;
 	public PanePrincipal panePrincipal;
-	
+
 	private FSA_state etat_courant;
 	private int i_joueur_courant;
 	private long timeA;
 	private float vitesseDelais = 1f;
-	private int delayDebutIA=(int) (250/vitesseDelais);
-	private int delayEntreTour=(int) (250/vitesseDelais);
-	private int delayCiblage=(int) (500/vitesseDelais);
+	private int delayDebutIA = (int) (250 / vitesseDelais);
+	private int delayEntreTour = (int) (250 / vitesseDelais);
+	private int delayCiblage = (int) (500 / vitesseDelais);
 	private Joueur[] joueurs;
 	private Color[] couleursJoueurs;
 	private CibleurGraphique cibleur;
@@ -39,60 +39,66 @@ public class Moteur {
 		etat_courant = FSA_state.DEBUT_TOUR;
 		joueurs = new Joueur[2];
 		joueurs[0] = new JoueurPhysique();
-		//joueurs[0] = new JoueurIA(panePrincipal.plateau, Joueur.Difficulte.FACILE);
+		// joueurs[0] = new JoueurIA(panePrincipal.plateau,
+		// Joueur.Difficulte.FACILE);
 		joueurs[1] = new JoueurIA(panePrincipal.plateau, Joueur.Difficulte.FACILE);
-		//joueurs[1] = new JoueurPhysique();
+		joueurs[1] = new JoueurPhysique();
 		couleursJoueurs = new Color[2];
-		couleursJoueurs[0] = new Color(100f/255f,160f/255f,50f/255f,1);
-		couleursJoueurs[1] = new Color(230f/255f,60f/255f,60f/255f,1);
+		couleursJoueurs[0] = new Color(100f / 255f, 160f / 255f, 50f / 255f, 1);
+		couleursJoueurs[1] = new Color(230f / 255f, 60f / 255f, 60f / 255f, 1);
 		cibleur = new CibleurGraphique(panePrincipal.gameView.plateauGraphique(), 0, 0);
 		panePrincipal.gameView.gameObjects.add(cibleur);
 		panePrincipal.enteteView.label.setText(nom_joueur(i_joueur_courant));
 		panePrincipal.enteteView.label.setTextFill(couleur_courante());
 	}
-	
+
 	public void update() {
 		switch (etat_courant) {
 		case DEBUT_TOUR:
-			if(!joueurs[i_joueur_courant].estIA() || timeA+delayDebutIA<System.currentTimeMillis()) {
+			if (!joueurs[i_joueur_courant].estIA() || timeA + delayDebutIA < System.currentTimeMillis()) {
 				etat_courant = FSA_state.CHOISIR_COUP;
 				cibleur.setCouleur(couleur_courante());
 			}
-			if(!joueurs[i_joueur_courant].estIA()) {
+			if (!joueurs[i_joueur_courant].estIA()) {
 				panePrincipal.gameView.cibleurGraphique().setCouleur(couleur_courante());
 			}
 			break;
 		case CHOISIR_COUP:
 			if (joueurs[i_joueur_courant].estIA()
 					&& panePrincipal.plateau.estMangeable(panePrincipal.plateau.getCasePoison())) {
-						coupIA = joueurs[i_joueur_courant].prochainCoup(panePrincipal.plateau);
-						cibleur.setCaseCible(coupIA);
-						etat_courant = FSA_state.CASE_CIBLE;
-						timeA = System.currentTimeMillis();
+				coupIA = joueurs[i_joueur_courant].prochainCoup(panePrincipal.plateau);
+				cibleur.setCaseCible(coupIA);
+				etat_courant = FSA_state.CASE_CIBLE;
+				timeA = System.currentTimeMillis();
 			}
 			break;
 		case CASE_CIBLE:
-			if(timeA+delayCiblage<System.currentTimeMillis()) {
-				cibleur.setCaseCible(new Couple(-1,-1));
+			if (timeA + delayCiblage < System.currentTimeMillis()) {
+				cibleur.setCaseCible(new Couple(-1, -1));
 				panePrincipal.plateau.manger(coupIA);
 				etat_courant = FSA_state.FIN_COUP;
 			}
 			break;
-			
+
 		case FIN_COUP:
 			timeA = System.currentTimeMillis();
 			etat_courant = FSA_state.FIN_TOUR;
 			break;
 		case FIN_TOUR:
-			if(timeA+delayEntreTour<System.currentTimeMillis()) {
-				etat_courant = FSA_state.DEBUT_TOUR;
+			if (timeA + delayEntreTour < System.currentTimeMillis()) {
+				if (!panePrincipal.plateau.estMangeable(panePrincipal.plateau.getCasePoison()))
+					etat_courant = FSA_state.FIN_PARTIE;
+				else
+					etat_courant = FSA_state.DEBUT_TOUR;
 				timeA = System.currentTimeMillis();
 				joueur_suivant();
 			}
 			break;
+		case FIN_PARTIE:
+			break;
 		}
 	}
-	
+
 	public void setEtatCourant(FSA_state s) {
 		etat_courant = s;
 	}
@@ -108,7 +114,7 @@ public class Moteur {
 	public Color couleur_courante() {
 		return couleursJoueurs[i_joueur_courant];
 	}
-	
+
 	private String nom_joueur(int n) {
 		if (joueurs[0].estIA() && joueurs[1].estIA()) {
 			return "Ordinateur " + (n + 1);
@@ -121,39 +127,39 @@ public class Moteur {
 				return "Joueur";
 		}
 	}
-	
+
 	private void joueur_suivant() {
-		i_joueur_courant = 1-i_joueur_courant;
+		i_joueur_courant = 1 - i_joueur_courant;
 		panePrincipal.enteteView.label.setText(nom_joueur(i_joueur_courant));
 		panePrincipal.enteteView.label.setTextFill(couleur_courante());
 	}
-	
+
 	public void undo() {
-		if(!joueurs[i_joueur_courant].estIA()) {
-			if(joueurs[1-i_joueur_courant].estIA()) {
+		if (!joueurs[i_joueur_courant].estIA()) {
+			if (joueurs[1 - i_joueur_courant].estIA()) {
 				panePrincipal.plateau.undo();
 				panePrincipal.plateau.undo();
+			} else {
+				if (panePrincipal.plateau.undo())
+					joueur_suivant();
 			}
-			else {
-				panePrincipal.plateau.undo();
-				joueur_suivant();
-			}
+			etat_courant = FSA_state.CHOISIR_COUP;
 		}
 	}
-	
+
 	public void redo() {
-		if(!joueurs[i_joueur_courant].estIA()) {
-			if(joueurs[1-i_joueur_courant].estIA()) {
+		if (!joueurs[i_joueur_courant].estIA()) {
+			if (joueurs[1 - i_joueur_courant].estIA()) {
 				panePrincipal.plateau.redo();
 				panePrincipal.plateau.redo();
+			} else {
+				if (panePrincipal.plateau.redo())
+					joueur_suivant();
 			}
-			else {
-				panePrincipal.plateau.redo();
-				joueur_suivant();
-			}
+			etat_courant = FSA_state.CHOISIR_COUP;
 		}
 	}
-	
+
 	public void setVitesseDelais(float v) {
 		vitesseDelais = v;
 	}
